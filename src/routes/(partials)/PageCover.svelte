5caf202c1f3440701e1fa7a6e4e3aa17df88e08a
Aaron Happe <aaronmichaelhappe@gmail.com>
2023-09-13 15:23:12 -0700

    style: minor styling adjustments

--- src/routes/(partials)/PageCover.svelte
+++ src/routes/(partials)/PageCover.svelte
@@ -1,20 +1,20 @@
-<div class="relative h-screen">
+<div class="relative h-screen bg-surface-800">
 	<div class="absolute inset-0 z-0 h-screen bg-surface-800" />
-	<div class="container relative p-2 pt-12 mx-auto sm:pt-16">
+	<div class="container relative p-2 py-12 mx-auto sm:pt-16">
 		<h3
-			class="font-bold uppercase text-surface-50 text-5xl leading-5xl sm:text-[10vw] sm:leading-[10vw] xl:text-9xl xl:leading-9xl"
+			class="font-bold uppercase text-surface-50 text-6xl leading-6xl sm:text-[10vw] sm:leading-[10vw] xl:text-9xl xl:leading-9xl"
 		>
 			UX.
 		</h3>
 		<h3
-			class="font-bold uppercase text-surface-50 text-5xl leading-5xl sm:text-[10vw] sm:leading-[10vw] xl:text-9xl xl:leading-9xl"
+			class="font-bold uppercase text-surface-50 text-6xl leading-6xl sm:text-[10vw] sm:leading-[10vw] xl:text-9xl xl:leading-9xl"
 		>
 			Design.
 		</h3>
 		<h3
-			class="font-bold uppercase text-surface-50 text-5xl leading-5xl sm:text-[10vw] sm:leading-[10vw] xl:text-9xl xl:leading-9xl"
+			class="font-bold uppercase text-surface-50 text-6xl leading-6xl sm:text-[10vw] sm:leading-[10vw] xl:text-9xl xl:leading-9xl"
 		>
-			Development.
+			Dev<span class="hidden sm:inline">elopment</span>.
 		</h3>
 		<p
 			class="sm:pl-2 pl-1 xl:text-4xl xl:leading-4xl sm:text-[4vw] sm:leading-[4vw] text-2xl leading-base-2xl text-surface-50"
@@ -23,7 +23,7 @@
 		</p>
 	</div>
 	<h4
-		class="absolute bottom-0 left-0 right-0 flex items-center w-full text-4xl bg-surface-50 text-primary-500 lg:text-6xl lg:leading-6xl sm:text-5xl sm:leading-5xl leading-4xl"
+		class="fixed bottom-0 left-0 right-0 flex items-center w-full text-4xl bg-surface-50 text-primary-500 lg:text-6xl lg:leading-6xl sm:text-5xl sm:leading-5xl leading-4xl"
 	>
 		<span class="container inline-block p-4 mx-auto sm:py-4 sm:p-2">Work</span>
 	</h4>
