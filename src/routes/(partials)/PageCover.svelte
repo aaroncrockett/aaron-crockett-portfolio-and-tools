9ee208cdb263d824762ddff71f2dc5e9a8495a23
Aaron Happe <aaronmichaelhappe@gmail.com>
2023-09-16 16:04:35 -0700

    feat: intro animations

--- src/routes/(partials)/PageCover.svelte
+++ src/routes/(partials)/PageCover.svelte
@@ -1,30 +1,84 @@
-<div class="relative h-screen bg-surface-800">
-	<div class="absolute inset-0 z-0 h-screen bg-surface-800" />
+<script lang="ts">
+	// Svelte
+	import { onMount } from 'svelte';
+	import { fly } from 'svelte/transition';
+	import { backOut, cubicOut } from 'svelte/easing';
+	// Other
+	import classNames from 'classnames';
+
+	const headlinesBucket = ['WEB', 'APPLICATION', 'DEVELOPER'];
+
+	let headlinesTransitionsHaveEnded = false;
+	let triggerOnMountAnimations = false;
+	let headlines: string[] = [];
+
+	onMount(() => {
+		setTimeout(() => {
+			triggerOnMountAnimations = true;
+			let index = 0;
+			// Create headline line in chunks from headlinesBucket to animate in.
+			const interval = setInterval(() => {
+				if (index < headlinesBucket.length) {
+					headlines = [...headlines, headlinesBucket[index]];
+					index++;
+				} else {
+					headlinesTransitionsHaveEnded = true;
+					clearInterval(interval);
+				}
+			}, 450);
+		}, 400);
+	});
+</script>
+
+<!-- Remove scroll bars with overflow-hidden until animations are finished to avoid layout shift -->
+<div
+	class={classNames(
+		'relative h-screen',
+		`${headlinesTransitionsHaveEnded ? 'overflow-auto' : 'overflow-hidden'}`
+	)}
+>
+	<!-- Transition background intro. -->
+	{#if triggerOnMountAnimations}
+		<div
+			class={`absolute inset-0 z-100 h-screen bg-surface-600`}
+			in:fly={{ y: '100%', easing: cubicOut }}
+		/>
+	{/if}
 	<div class="container relative p-2 py-12 mx-auto sm:pt-16">
-		<h3
-			class="font-bold uppercase text-surface-50 text-6xl leading-6xl sm:text-[10vw] sm:leading-[10vw] xl:text-9xl xl:leading-9xl"
-		>
-			WEB
-		</h3>
-		<h3
-			class="font-bold uppercase text-surface-50 text-6xl leading-6xl sm:text-[10vw] sm:leading-[10vw] xl:text-9xl xl:leading-9xl"
-		>
-			APPLICATION
-		</h3>
-		<h3
-			class="font-bold uppercase text-surface-50 text-6xl leading-6xl sm:text-[10vw] sm:leading-[10vw] xl:text-9xl xl:leading-9xl"
-		>
-			DEVELOPER
-		</h3>
-		<p
-			class="sm:pl-2 pl-1 xl:text-4xl xl:leading-4xl sm:text-[4vw] sm:leading-[4vw] text-2xl leading-base-2xl text-surface-50"
-		>
-			Frontend focus with fullstack experience.
-		</p>
+		<div class="flex flex-col overflow-hidden">
+			{#each headlines as line}
+				<h3
+					class="font-bold uppercase text-surface-50 text-6xl leading-6xl sm:text-[10vw] sm:leading-[10vw] xl:text-9xl xl:leading-9xl"
+					in:fly={{
+						y: 150,
+						easing: backOut
+					}}
+				>
+					{line}
+				</h3>
+			{/each}
+		</div>
+		{#if headlinesTransitionsHaveEnded}
+			<p
+				class="sm:pl-2 pl-1 xl:text-4xl xl:leading-4xl sm:text-[4vw] sm:leading-[4vw] text-2xl leading-base-2xl text-surface-50"
+				in:fly={{
+					x: '-100%',
+					easing: cubicOut
+				}}
+			>
+				Front-End focus with Full-Stack experience.
+			</p>
+		{/if}
 	</div>
+
 	<h4
-		class="fixed bottom-0 left-0 right-0 flex items-center w-full text-4xl bg-surface-50 text-primary-500 lg:text-6xl lg:leading-6xl sm:text-5xl sm:leading-5xl leading-4xl"
+		class={`z-5 absolute flex w-full items-center bottom-0 right-0 left-0 bg-surface-50 text-primary-500 sm:text-3xl sm:leading-3xl text-2xl leading-2xl`}
 	>
-		<span class="container inline-block p-4 mx-auto sm:py-4 sm:p-2">Work</span>
+		<span
+			class={`${
+				triggerOnMountAnimations ? 'opacity-100' : 'opacity-0'
+			} transition-all duration-600 container inline-block p-4 mx-auto sm:py-4 sm:p-2`}
+			>Interactive Curriculum Vitae</span
+		>
 	</h4>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,49 +1,76 @@
 <script lang="ts">
+	// CSS
 	import '../app.postcss';
+	// Svelte
+	import { onMount } from 'svelte';
+	import type { ComponentEvents } from 'svelte';
+	import { fade } from 'svelte/transition';
+	import { cubicIn } from 'svelte/easing';
+	// Skeleton Labs
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	// Other
+	import classNames from 'classnames';
+
+	let triggerOnMountTransitions = false;
+
+	let appBarWrapperElBg = '';
+
+	function scrollHandler(event: ComponentEvents<AppShell>['scroll']) {
+		if (event.currentTarget.scrollTop > 10) {
+			appBarWrapperElBg = 'bg-surface-600 shadow-sm';
+		}
+	}
+
+	onMount(() => {
+		triggerOnMountTransitions = true;
+	});
 </script>
 
 <!-- App Shell -->
-<AppShell>
+<AppShell on:scroll={scrollHandler}>
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
-		<div class="bg-surface-800">
-			<AppBar class="container mx-auto" background="bg-surface-800 fixed top-0 right-0 left-0">
-				<svelte:fragment slot="lead">
-					<div class="text-2xl sm:text-4xl sm:leading-4xl leading-2xl text-secondary-500">
-						<span class="uppercase text-primary-500">AARON</span><span
-							class="uppercase text-tertiary-500">CROCKETT</span
+		{#if triggerOnMountTransitions}
+			<div
+				transition:fade={{ easing: cubicIn, duration: 800 }}
+				class={classNames('fixed top-0 right-0 left-0', `${appBarWrapperElBg}`)}
+			>
+				<AppBar class="container mx-auto" background="bg-none">
+					<svelte:fragment slot="lead">
+						<div class="text-2xl uppercase sm:text-3xl sm:leading-3xl leading-2xl">
+							<span class="text-primary-500">AARON</span>
+							<span class="text-tertiary-500">HAPPE</span>
+						</div>
+					</svelte:fragment>
+					<svelte:fragment slot="trail">
+						<a
+							class="hidden text-white btn sm:inline-block variant-ghost-primary"
+							href="https://github.com/aaronmichaelhappe/portolio-skeleton-ui"
+							target="_blank"
+							rel="noreferrer"
 						>
-					</div>
-				</svelte:fragment>
-				<svelte:fragment slot="trail">
-					<a
-						class="hidden text-white btn sm:inline-block variant-ghost-primary"
-						href="https://github.com/aaronmichaelhappe/portolio-skeleton-ui"
-						target="_blank"
-						rel="noreferrer"
-					>
-						Discord
-					</a>
-					<a
-						class="hidden font-bold text-white btn sm:inline-block variant-ghost-primary"
-						href="https://github.com/aaronmichaelhappe/portolio-skeleton-ui"
-						target="_blank"
-						rel="noreferrer"
-					>
-						Twitter
-					</a>
-					<a
-						class="hidden font-bold text-white btn sm:inline-block variant-ghost-primary"
-						href="https://github.com/aaronmichaelhappe/portolio-skeleton-ui"
-						target="_blank"
-						rel="noreferrer"
-					>
-						GitHub
-					</a>
-				</svelte:fragment>
-			</AppBar>
-		</div>
+							Discord
+						</a>
+						<a
+							class="hidden font-bold text-white btn sm:inline-block variant-ghost-primary"
+							href="https://github.com/aaronmichaelhappe/portolio-skeleton-ui"
+							target="_blank"
+							rel="noreferrer"
+						>
+							Twitter
+						</a>
+						<a
+							class="hidden font-bold text-white btn sm:inline-block variant-ghost-primary"
+							href="https://github.com/aaronmichaelhappe/portolio-skeleton-ui"
+							target="_blank"
+							rel="noreferrer"
+						>
+							GitHub
+						</a>
+					</svelte:fragment>
+				</AppBar>
+			</div>
+		{/if}
 	</svelte:fragment>
 	<!-- Page Route Content -->
 	<slot />
