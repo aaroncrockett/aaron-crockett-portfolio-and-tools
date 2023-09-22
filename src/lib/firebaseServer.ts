3b41551a10b366e6721f0bb4fabc95aba8a5c63d
Aaron Happe <aaronmichaelhappe@gmail.com>
2023-09-22 10:54:39 -0700

    temp server exmaple

--- src/lib/firebaseServer.ts
+++ src/lib/firebaseServer.ts
@@ -1,10 +1,18 @@
 import { initializeApp } from 'firebase-admin/app';
 import { credential as adminCredential } from 'firebase-admin';
+import { getAuth } from 'firebase-admin/auth';
+import { getFirestore } from 'firebase-admin/firestore';
+import fs from 'fs'; // Import the Node.js file system module
 
 import { GOOGLE_APPLICATION_CREDENTIALS } from '$env/static/private';
 
-const serviceAccount = JSON.parse(GOOGLE_APPLICATION_CREDENTIALS);
+console.log('GOOGLE_APPLICATION_CREDENTIALS ', GOOGLE_APPLICATION_CREDENTIALS);
 
-initializeApp({
+const serviceAccount = JSON.parse(fs.readFileSync(GOOGLE_APPLICATION_CREDENTIALS, 'utf8'));
+
+const app = initializeApp({
 	credential: adminCredential.cert(serviceAccount)
 });
+
+export const firestore = getFirestore(app);
+export const auth = getAuth(app);
