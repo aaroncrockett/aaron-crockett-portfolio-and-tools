27678aaafd966eccebeb25c29d5dce74fe8e4246
Aaron Crockett <aaron.crockett.00@gmail.com>
2023-09-30 15:50:08 -0700

    wip: adjust gpt questions and related

--- src/lib/questions.ts
+++ src/lib/questions.ts
@@ -3,8 +3,12 @@ export const gptQuestions = {
 		'How can a candidate with a background in art and design as well as front end development assist my team?',
 		'The candidate has a background in art and design as well as front end development. Give me a short 3 to 4 sentence response about how the candidates back ground can assist my team with collaboration and creative problem solving.'
 	],
-	'vue-certification': [
-		'Who is Evan You and what is the Vue.js Certification?',
-		'Who is Evan You? The Candidate took an offial Vue.js Certification. The Certification was approved by Evan You himself. What does it say about a candidate that obtained such a certification? Give me a short 2 ot 3 sentence response.'
+	'open-source': [
+		'What is open source software and what does it say about a developer who contributes to it?',
+		'What is open source software and what does it say about a developer who contributes to it? Give me a short 2 ot 3 sentence response.'
+	],
+	'diverse-companies': [
+		'What does it say about a developer who has worked for highly visible brands with larger teams, and also, worked on small nimble teams of 3 people?',
+		'What does it say about a developer who has worked for highly visible brands with larger teams, and also, worked on small nimble teams of 3 people? Give a short 2 to 3 sentence response.'
 	]
 };
