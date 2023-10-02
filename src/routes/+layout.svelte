6807d0424a81fb5c3996cb7f871e092019abfa88
Aaron Crockett <aaron.crockett.00@gmail.com>
2023-10-01 19:57:57 -0700

    feat: footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -81,5 +81,16 @@
 		</svelte:fragment>
 		<!-- Page Route Content -->
 		<slot />
+		<svelte:fragment slot="footer">
+			<div class="p-2 bg-surface-500">
+				<div class="sm:inline-block">
+					<a href="/">
+						<span class="text-primary-500">AARON</span>
+						<span class="text-tertiary-500">CROCKETT</span> |
+					</a>
+				</div>
+				<div class="sm:inline-block"><span class="text-white">WEB APPLICATION DEVELOPER</span></div>
+			</div>
+		</svelte:fragment>
 	</AppShell>
 </div>
