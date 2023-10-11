ad76d19f82d5e341e98e7ef23962e8df6181bd81
Aaron Crockett <aaron.crockett.00@gmail.com>
2023-10-11 13:10:02 -0700

    feature: referral section

--- src/routes/(partials)/References.svelte
+++ src/routes/(partials)/References.svelte
@@ -1,29 +1,101 @@
 <section class="page-section">
 	<h3
-		class="uppercase font-bold text-2xl sm:text-3xl sm:leading-3xl leading-3xl text-center pb-4 pt-1 rounded"
+		class="uppercase font-bold text-2xl sm:text-3xl sm:leading-3xl leading-3xl text-center pb-2 pt-1 rounded"
 	>
-		References
+		testimonials
 	</h3>
 	<p class="text-xl text-center">
-		References given on online platforms (Linked In and Zip Recruiter) gathered here in one place.
+		Recommendations given on online platforms (Linked In and Zip Recruiter) gathered here in one
+		place.
 	</p>
 
 	<div class="flex flex-col gap-4">
-		<div class="flex flex-col sm:w-2/3 mx-auto variant-soft-surface p-1 card">
-			<p class="text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-			<p>Kathryn Janeway</p>
+		<div class="flex flex-col gap-1 lg:w-2/3 mx-auto variant-soft-surface card p-4">
+			<blockquote
+				class="referral border-l-[10px] pl-4 border-surface-400 variant-soft-surface card mb-2"
+			>
+				<p class=" p-2 pb-4 leading-2xl">
+					I had the honor of getting to work with Aaron early in his career. He was always an avid
+					learner and a dedicated developer. It's been so exciting to see him grow in his expertise
+					and experience. I would highly recommend him for your Front End focused team!
+				</p>
+			</blockquote>
+			<p class="text-xl font-bold inline-block leading-2xl">Director, Global Experience Strategy</p>
+			<p class="text-xl inline-block leading-xl">Jacob White</p>
 		</div>
-		<div class="flex flex-col sm:w-2/3 mx-auto variant-soft-surface p-1 card">
-			<p class="text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-			<p>Kathryn Janeway</p>
+		<div class="flex flex-col gap-1 lg:w-2/3 mx-auto variant-soft-surface card p-4">
+			<blockquote
+				class="referral border-l-[10px] pl-4 border-surface-400 variant-soft-surface card mb-2"
+			>
+				<p class="p-2 pb-4 leading-2xl">
+					Aaron did a good job updating our entire website (switching to a more modern style) and
+					also working on our product's web interface, converting it from JavaScript (and ext.js) to
+					Vue. He's a very knowledgeable developer.
+				</p>
+			</blockquote>
+			<p class="text-xl font-bold inline-block leading-2xl">Founder at Power Admin LLC</p>
+			<p class="text-xl inline-block leading-xl">Doug Nebeker</p>
 		</div>
-		<div class="flex flex-col sm:w-2/3 mx-auto variant-soft-surface p-1 card">
-			<p class="text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-			<p>Kathryn Janeway</p>
+		<div class="flex flex-col gap-1 lg:w-2/3 mx-auto variant-soft-surface card p-4">
+			<blockquote
+				class="referral border-l-[10px] pl-4 border-surface-400 variant-soft-surface card mb-2"
+			>
+				<p class="p-2 pb-4 leading-2xl">
+					I had the benefit of working side-by-side with Aaron as a frontend web developer at
+					Caleres for a number of years. He is thoughtful and creative with his problem solving, and
+					he puts in the effort to make sure his work is always quality down to the detail. His
+					pressence at work was a joy as he always had something nice to say or a story to share. He
+					would be a wonderful addition to any team seeking his talents!
+				</p>
+			</blockquote>
+			<p class="text-xl font-bold inline-block leading-3xl">
+				Analytics Implementation Engineer, Pluto TV
+			</p>
+			<p class="text-xl inline-block leading-xl">Henry Leacock</p>
 		</div>
-		<div class="flex flex-col sm:w-2/3 mx-auto variant-soft-surface p-1 card">
-			<p class="text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-			<p>Kathryn Janeway</p>
+		<div class="flex flex-col gap-1 lg:w-2/3 mx-auto variant-soft-surface card p-4">
+			<blockquote
+				class="referral border-l-[10px] pl-4 border-surface-400 variant-soft-surface card mb-2"
+			>
+				<p class="p-2 pb-4 leading-2xl">
+					I had the pleasure of being a project manager on a team with Aaron as a front-end
+					developer and cannot recommend him enough. He has great attention to detail and accuracy,
+					communicates professionally, and always delivered his projects on time. In addition to
+					that, he's a supportive and friendly team member. Would love to work with him again!
+				</p>
+			</blockquote>
+			<p class="text-xl font-bold inline-block leading-2xl">Project Manager, Bed Bath & Beyond</p>
+			<p class="text-xl inline-block leading-xl">Morgan Markus</p>
+		</div>
+		<div class="flex flex-col gap-1 lg:w-2/3 mx-auto variant-soft-surface card p-4">
+			<blockquote
+				class="referral border-l-[10px] pl-4 border-surface-400 variant-soft-surface card mb-2"
+			>
+				<p class="p-2 pb-4 leading-2xl">
+					Aaron did an excellent job working for us and was an excellent coworker at Caleres too. He
+					is a talented developer and easy to get along with. I highly recommend him for your
+					development and design needs.
+				</p>
+			</blockquote>
+			<p class="text-xl font-bold inline-block leading-2xl">Founder at Madness Labs</p>
+			<p class="text-xl inline-block leading-xl">Bobby Johnson</p>
+		</div>
+		<div class="flex flex-col gap-1 lg:w-2/3 mx-auto variant-soft-surface card p-4">
+			<blockquote
+				class="referral border-l-[10px] pl-4 border-surface-400 variant-soft-surface card mb-2"
+			>
+				<p class="p-2 pb-4 leading-2xl">
+					As a volunteer Aaron proactively contacted me to complete a redesign of Campbell House
+					Museum's website (we are a small non-profit). Over a period of about 5 months Aaron worked
+					to create a new concept in updating the web presence for Campbell House that would be easy
+					to maintain. He exceeded our expectations and was a pleasure to work with, highly
+					recommended!
+				</p>
+			</blockquote>
+			<p class="text-xl font-bold inline-block leading-2xl">
+				Executive Director at Campbell House Museum
+			</p>
+			<p class="text-xl inline-block leading-xl">Andrew Hahn</p>
 		</div>
 	</div>
 </section>
