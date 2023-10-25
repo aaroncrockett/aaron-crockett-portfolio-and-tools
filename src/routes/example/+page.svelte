f6f159756e1d7fdea329ddb689cb2cf874e0e3f1
Aaron Crockett <aaron.crockett.00@gmail.com>
2023-10-25 09:56:44 -0700

    chore: init inner page

--- src/routes/example/+page.svelte
+++ src/routes/example/+page.svelte
@@ -1,1 +1,14 @@
-<div><p>example page</p></div>
+<script>
+	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
+	import { cubicIn } from 'svelte/easing';
+
+	let triggerOnMountTransitions = false;
+	onMount(() => {
+		triggerOnMountTransitions = true;
+	});
+</script>
+
+{#if triggerOnMountTransitions}
+	<div class="page" transition:fade={{ easing: cubicIn, duration: 800 }}><p>example page</p></div>
+{/if}
