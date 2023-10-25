d24f0b77d903c0a8951b4d6d1cc09fd49862b938
Aaron Crockett <aaron.crockett.00@gmail.com>
2023-10-25 16:25:54 -0700

    bug: missing close nav on home click

--- src/lib/components/DrawerMenuContents.svelte
+++ src/lib/components/DrawerMenuContents.svelte
@@ -24,7 +24,7 @@
 <div class="flex">
 	<div class="flex flex-col gap-2 p-2">
 		<div class="border-b border-primary-500">
-			<a on:click class="inline-block p-1 sm:p-2" href="/">
+			<a on:click={() => drawerStore.close()} class="inline-block p-1 sm:p-2" href="/">
 				<span class="hidden sm:inline-block">
 					{@html homeIconSvg}
 				</span>
