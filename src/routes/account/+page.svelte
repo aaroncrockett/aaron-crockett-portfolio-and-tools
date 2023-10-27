b8c69e4e40cbc9a1eef688ec528fa0db6f445279
Aaron Crockett <aaron.crockett.00@gmail.com>
2023-10-27 12:29:54 -0700

    chore: remove page signout and minor refactoring

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -19,65 +19,52 @@
 			loading = false;
 		};
 	};
-
-	// const handleSignOut: SubmitFunction = () => {
-	// 	loading = true;
-	// 	return async ({ update }) => {
-	// 		loading = false;
-	// 		update();
-	// 	};
-	// };
 </script>
 
 <div class="page-one-col">
 	<h2 class="page-header">Account</h2>
-	<div class="sm:w-2/3 md:w-1/2 w-full flex flex-col gap-2">
-		<form
-			class="flex flex-col gap-2"
-			method="post"
-			action="?/update"
-			use:enhance={handleSubmit}
-			bind:this={profileForm}
-		>
-			<div>
-				<label for="email">Email</label>
-				<input id="email" class="input" type="text" value={session.user.email} disabled />
-			</div>
 
-			<div>
-				<label for="fullName">Full Name</label>
-				<input
-					id="fullName"
-					class="input"
-					name="fullName"
-					type="text"
-					value={form?.fullName ?? fullName}
-				/>
-			</div>
+	<form
+		class="flex flex-col gap-4 sm:w-2/3 md:w-1/2 w-full"
+		method="post"
+		action="?/update"
+		use:enhance={handleSubmit}
+		bind:this={profileForm}
+	>
+		<div>
+			<label for="email">Email</label>
+			<input id="email" class="input" type="text" value={session.user.email} disabled />
+		</div>
 
-			<div>
-				<label for="username">Username</label>
-				<input
-					id="username"
-					class="input"
-					name="username"
-					type="text"
-					value={form?.username ?? username}
-				/>
-			</div>
+		<div>
+			<label for="fullName">Full Name</label>
+			<input
+				id="fullName"
+				class="input"
+				name="fullName"
+				type="text"
+				value={form?.fullName ?? fullName}
+			/>
+		</div>
 
-			<div>
-				<input
-					type="submit"
-					class="btn btn-sm variant-ghost-secondary block"
-					value={loading ? 'Loading...' : 'Update'}
-					disabled={loading}
-				/>
-			</div>
-		</form>
+		<div>
+			<label for="username">Username</label>
+			<input
+				id="username"
+				class="input"
+				name="username"
+				type="text"
+				value={form?.username ?? username}
+			/>
+		</div>
 
-		<form method="post" action="sign-out">
-			<button class="btn variant-ghost-primary" disabled={loading}>Sign Out</button>
-		</form>
-	</div>
+		<div>
+			<input
+				type="submit"
+				class="btn variant-ghost-secondary block"
+				value={loading ? 'Loading...' : 'Update'}
+				disabled={loading}
+			/>
+		</div>
+	</form>
 </div>
