320b974d095b963198a6b29abf9147c3c73ab466
Aaron Crockett <aaron.crockett.00@gmail.com>
2023-10-27 12:29:01 -0700

    remove supabase component and replace w my styles

--- src/routes/sign-in/+page.svelte
+++ src/routes/sign-in/+page.svelte
@@ -3,16 +3,25 @@
 	import { ThemeSupa } from '@supabase/auth-ui-shared';
 
 	export let data;
+
+	let email = '';
+
+	async function sendMagicLink() {
+		const { data: localData, error } = await data.supabase.auth.signInWithOtp({
+			email: email,
+			options: {
+				emailRedirectTo: `${data.url}/auth/callback`
+			}
+		});
+	}
 </script>
 
 <div class="page-one-col">
 	<div class=" sm:w-2/3 md:w-1/2 w-full">
-		<Auth
-			supabaseClient={data.supabase}
-			view="magic_link"
-			redirectTo={`${data.url}/auth/callback`}
-			showLinks={false}
-			appearance={{ theme: ThemeSupa, style: { input: 'color: #fff' } }}
-		/>
+		<div class="flex flex-col gap-2">
+			<label for="email">Email</label>
+			<input bind:value={email} id="email" class="input" type="text" />
+			<button class="btn variant-ghost-primary" on:click={sendMagicLink}> Send Magic Link </button>
+		</div>
 	</div>
 </div>
