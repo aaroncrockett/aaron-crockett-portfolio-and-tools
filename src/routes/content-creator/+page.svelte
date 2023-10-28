6279f0a11464392b1e731391105e0dbfd105ce07
Aaron Crockett <aaron.crockett.00@gmail.com>
2023-10-27 18:57:15 -0700

    chore: tiptap color functionality

--- src/routes/content-creator/+page.svelte
+++ src/routes/content-creator/+page.svelte
@@ -5,18 +5,29 @@
 	import type { Readable } from 'svelte/store';
 	import { createEditor, Editor, EditorContent, BubbleMenu } from 'svelte-tiptap';
 	import StarterKit from '@tiptap/starter-kit';
+	import { Heading } from '@tiptap/extension-heading';
+	import { Color } from '@tiptap/extension-color';
+	import TextStyle from '@tiptap/extension-text-style';
 
 	let triggerOnMountTransitions = false;
-
 	let editor: Readable<Editor>;
 
 	onMount(() => {
 		triggerOnMountTransitions = true;
 		editor = createEditor({
-			extensions: [StarterKit],
+			extensions: [
+				StarterKit,
+				Heading.configure({
+					HTMLAttributes: {
+						levels: [1, 2]
+					}
+				}),
+				TextStyle,
+				Color
+			],
 			editorProps: {
 				attributes: {
-					class: 'p-2 variant-soft-surface rounded'
+					class: 'p-4 variant-soft-surface rounded'
 				}
 			},
 			content: `Hello world! Edit this content!`
@@ -34,23 +45,25 @@
 		<BubbleMenu editor={$editor}>
 			<div class={'bg-surface-700 rounded p-2 flex flex-col gap-2'}>
 				<div class="flex gap-1">
-					<button
-						class="context-button"
-						on:click={() => $editor.chain().focus().toggleBold().run()}
-					>
-						bold
+					<button class="context-button" on:click={() => $editor.commands.setHeading({ level: 1 })}>
+						h1
 					</button>
-					<button
-						class="context-button"
-						on:click={() => $editor.chain().focus().toggleItalic().run()}
-					>
-						italic
+					<button class="context-button" on:click={() => $editor.commands.setHeading({ level: 2 })}>
+						h2
 					</button>
 					<button
 						class="context-button"
 						on:click={() => $editor.chain().focus().toggleStrike().run()}
 					>
 						strike
+					</button>
+				</div>
+				<div class="flex gap-1">
+					<button
+						class="context-button"
+						on:click={() => $editor.chain().focus().setColor('#d7424b').run()}
+					>
+						Red
 					</button>
 				</div>
 			</div>
@@ -61,5 +74,11 @@
 <style lang="postcss">
 	.context-button {
 		@apply variant-soft-primary p-1 rounded;
+	}
+	:global(.tiptap h1) {
+		@apply !text-3xl font-bold;
+	}
+	:global(.tiptap h2) {
+		@apply !text-xl font-bold;
 	}
 </style>
