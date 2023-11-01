5cbc6a941d96ccf7c45b0d41ebfa4bd6c43d0ad1
Aaron Crockett <aaron.crockett.00@gmail.com>
2023-10-31 20:24:40 -0700

    feat:wip quiz maker

---
+++ src/routes/quiz-maker/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import QuizMaker from '$lib/components/Quiz/QuizMaker.svelte';
+
+	async function submitQuiz(event) {
+		console.log(event);
+	}
+</script>
+
+<div class="page-one-col">
+	<QuizMaker on:submit={submitQuiz} />
+</div>
