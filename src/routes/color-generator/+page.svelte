2ebdb05698a364c1e4f67d779ccad654892fd82b
Aaron Crockett <aaron.crockett.00@gmail.com>
2023-11-04 11:08:28 -0700

    wip: init feature

---
+++ src/routes/color-generator/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	let color = '#ff0000'; // Default color
+
+	function handleColorChange(event: Event) {
+		const input = event.target as HTMLInputElement;
+		color = input.value;
+	}
+</script>
+
+<section>
+	<h2 class="page-header">Color Generator</h2>
+	<input type="color" bind:value={color} on:input={handleColorChange} />
+
+	<!-- Display selected color -->
+	<div>
+		Selected color: <span style="background-color: {color};">{color}</span>
+	</div>
+</section>
