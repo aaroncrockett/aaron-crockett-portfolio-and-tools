72690797a77d152faa5669ac42f4d14be1ba6ced
Aaron Crockett <aaron.crockett.00@gmail.com>
2023-11-06 08:23:30 -0800

    chore: add email address

--- src/lib/components/SocialMediaLinks.svelte
+++ src/lib/components/SocialMediaLinks.svelte
@@ -1,33 +1,67 @@
 <script lang="ts">
 	import * as feather from 'feather-icons';
+	import { clipboard } from '@skeletonlabs/skeleton';
+
+	const emailAddress: string = 'aaron.crockett.00@gmail.com';
 
 	const gitIconSvg = feather.icons['github'].toSvg({
 		stroke: '#d7424b',
 		width: 28,
 		height: 28
 	});
-
 	const linkedIconSvg = feather.icons['linkedin'].toSvg({
 		stroke: '#d7424b',
 		width: 28,
 		height: 28
 	});
-	const gitIconSvgSm = feather.icons['github'].toSvg({
+	const mailIconSvg = feather.icons['mail'].toSvg({
 		stroke: '#d7424b',
-		width: 24,
-		height: 24
+		width: 28,
+		height: 28
 	});
 
+	const gitIconSvgSm = feather.icons['github'].toSvg({
+		stroke: '#d7424b',
+		width: 22,
+		height: 22
+	});
 	const linkedIconSvgSm = feather.icons['linkedin'].toSvg({
 		stroke: '#d7424b',
-		width: 24,
-		height: 24
+		width: 22,
+		height: 22
 	});
+	const mailIconSvgSm = feather.icons['mail'].toSvg({
+		stroke: '#d7424b',
+		width: 22,
+		height: 22
+	});
+
+	let copied = false;
+
+	function handleCopied() {
+		copied = true;
+		setTimeout(() => {
+			copied = false;
+		}, 1000);
+	}
 </script>
+
+<button on:click={handleCopied} class="inline-block sm:p-1" use:clipboard={emailAddress}>
+	{#if copied}
+		<span class="text-sm text-tertiary-500">COPIED 👍</span>
+	{:else}
+		<span class="hidden sm:inline">
+			{@html mailIconSvg}
+		</span>
+		<span class="inline sm:hidden">
+			{@html mailIconSvgSm}
+		</span>
+	{/if}
+</button>
 
 <a
 	on:click
-	class="inline-block p-1 sm:p-2"
+	class="inline-block sm:p-1"
 	href="https://github.com/aaroncrockett/"
 	target="_blank"
 	rel="noreferrer"
@@ -41,7 +75,7 @@
 </a>
 <a
 	on:click
-	class="p-1 sm:p-2 inline-block"
+	class=" sm:p-1 inline-block"
 	href="https://www.linkedin.com/in/aaron-happe-crockett-4741176a/"
 	target="_blank"
 	rel="noreferrer"
