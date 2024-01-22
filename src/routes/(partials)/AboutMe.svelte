ef5ecd19eee439be24d5435d30376f69ec468ccb
Aaron Crockett <aaron.crockett.00@gmail.com>
2024-01-22 06:43:58 -0800

    text update

--- src/routes/(partials)/AboutMe.svelte
+++ src/routes/(partials)/AboutMe.svelte
@@ -36,15 +36,14 @@
 		<div class="w-full space-y-2 sm:order-2 md:w-1/2">
 			<h3 class="text-primary-500">A little more about me.</h3>
 			<p>
-				I live in Portland, Oregon, with my husband Nick and our two dogs, Jasper and Fred. I enjoy
-				hiking, game nights with friends, and exploring local restaurants. Every morning, I start my
-				day by going for a jog either at the park or along the riverfront, followed by meditation,
-				before I begin work.
+				I live in Portland, Oregon, with my husband Nick and our two dogs, Jasper and Fred. Outside
+				of creating and engineering, I enjoy hiking, game nights with friends, and exploring local
+				restaurants. Every morning, I start my day by going for a jog either at the park or along
+				the riverfront, followed by meditation, before I begin work.
 			</p>
 			<p>
-				I have always had a passion for creating things. During my younger years, I enjoyed
-				tinkering with webpages. I also pursued my interests in painting, participated in design
-				contests, and even played in a couple of garage bands.
+				I have always had a passion for creating things. Earlier in life I pursued my interests in
+				painting, participated in design contests, and even played in a couple of garage bands.
 			</p>
 		</div>
 	</div>
